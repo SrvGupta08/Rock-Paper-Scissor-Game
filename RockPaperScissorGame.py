4086e1126e5d6448fd52c4db500a865326b93d9b
Sourav Gupta <srv.iem08@gmail.com>
2025-06-11 11:09:01 +0530

    Update RockPaperScissorGame.py

--- RockPaperScissorGame.py
+++ RockPaperScissorGame.py
@@ -30,28 +30,30 @@ scissor = '''
 '''
 game_images = [rock, paper, scissor]
 
-play_game = int(input("Press 1 to play the game!\nPress 2 to exit the game:"))
-if play_game == 1:
-    user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor.\n"))
-    if user_choice >=3 or user_choice < 0:
-        print("INVALID INPUT!")
-    else:
-        print("You chose\n")
-        print(game_images[user_choice])
-    
-        computer_choice = random.randint(0, 2)
-        print("Computer chose\n")
-        print(game_images[computer_choice])
-    
-        if user_choice == 0 and computer_choice == 2:
-            print("You win!")
-        elif user_choice == 1 and computer_choice == 0:
-            print("You win!")
-        elif user_choice == 2 and computer_choice == 1:
-            print("You win!")
-        elif user_choice == computer_choice:
-            print("It's a Draw!")
+while(True):
+    play_game = int(input("\nPress 1 to play the game!\nPress 2 to exit the game!\nEnter your choice: "))
+    if play_game == 1:
+        user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor.\n"))
+        if user_choice >=3 or user_choice < 0:
+            print("INVALID INPUT!")
         else:
-            print("You lose!")
-else:
-    print("Exiting...\nThank You!!")
+            print("You chose\n")
+            print(game_images[user_choice])
+        
+            computer_choice = random.randint(0, 2)
+            print("Computer chose\n")
+            print(game_images[computer_choice])
+        
+            if user_choice == 0 and computer_choice == 2:
+                print("You win!")
+            elif user_choice == 1 and computer_choice == 0:
+                print("You win!")
+            elif user_choice == 2 and computer_choice == 1:
+                print("You win!")
+            elif user_choice == computer_choice:
+                print("It's a Draw!")
+            else:
+                print("You lose!")        
+    else:
+        print("Exiting...\nThank You!!")
+        break
